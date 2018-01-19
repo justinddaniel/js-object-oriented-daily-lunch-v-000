883f52096bf7c98323bf2ccbb6bf79c3baa701e2
Justin Daniel <justinddaniel@gmail.com>
2018-01-19 02:47:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,57 +1,114 @@
-let store = {customers: [], meals: [], deliveries: [], employers: []}
-let customerId = 0
-let mealId = 0
-let deliveryId = 0
-let employerId = 0
+let store = { deliveries: [], employers: [], customers: [], meals: [] };
 
-class Customer {
-  constructor(name, employer) {
-    this.id = customerId++
-    if (name) {this.name = name}
-    if (employer) {this.employerId = employer.id}
-    store.customers.push(this)
-  }
-
-  totalSpent() {
-    let totalprice = 0
-    let custdel = store.deliveries.filter(delivery => {
-      return delivery.customerId == this.id})
-    custdel.forEach(function (element) {totalprice += element.meal.price})
-    return totalprice
-  }
-}
+let mealId = 0;
 
 class Meal {
   constructor(title, price) {
-  this.id = mealId++
-  this.title = title
-  this.price = price
-  store.meals.push(this)
-}
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+    store.meals.push(this);
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId == this.id;
+    });
+  }
+  customers() {
+    return this.deliveries().map(delivery => {
+      return delivery.customer();
+    });
+  }
   static byPrice() {
-    const priceSorter = function (meal1, meal2) {
-    return meal2.price - meal1.price;};
-    return store.meals.sort(priceSorter)
+    return store.meals.sort((meal1, meal2) => {
+      return meal1.price < meal2.price;
+    });
   }
 }
 
+let customerId = 0;
+class Customer {
+  constructor(name, employer = {}) {
+    this.name = name;
+    this.employerId = employer.id;
+    this.id = ++customerId;
+    store.customers.push(this);
+  }
+  totalSpent() {
+    return this.meals().reduce(function(sum, meal) {
+      return sum + meal.price;
+    }, 0);
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId == this.id;
+    });
+  }
+  meals() {
+    return this.deliveries().map(delivery => {
+      return delivery.meal();
+    });
+  }
+}
+
+let deliveryId = 0;
 class Delivery {
-  constructor(meal, customer) {
-    this.id = deliveryId++
-    if (meal) {this.mealId = meal.id; this.meal = meal}
-    if (customer) {this.customerId = customer.id}
-    store.deliveries.push(this)
+  constructor(meal = {}, customer = {}) {
+    this.mealId = meal.id;
+    this.customerId = customer.id;
+    this.id = ++deliveryId;
+    store.deliveries.push(this);
   }
-  customer() {return store.customers.filter(customer => {return customer.id == this.customerId})[0]
+  meal() {
+    return store.meals.find(meal => {
+      return meal.id === this.mealId;
+    });
   }
-  meal() {return store.meals.filter(meal => {return meal.id == this.mealId})[0]
+  customer() {
+    return store.customers.find(customer => {
+      return customer.id === this.customerId;
+    });
+  }
 }
-}
+
+let employerId = 0;
 
 class Employer {
   constructor(name) {
-    this.id = employerId++
-    this.name = name
-    store.employers.push(this)
+    this.name = name;
+    this.id = ++employerId;
+    store.employers.push(this);
+  }
+  mealTotals() {
+    let allMeals = this.deliveries().map(delivery => {
+      return delivery.meal();
+    });
+    let summaryObject = {};
+    allMeals.forEach(function(meal) {
+      summaryObject[meal.id] = 0;
+    });
+    allMeals.forEach(function(meal) {
+      summaryObject[meal.id] += 1;
+    });
+    return summaryObject;
+  }
+  employees() {
+    return store.customers.filter(customer => {
+      return customer.employerId == this.id;
+    });
+  }
+  deliveries() {
+    let allDeliveries = this.employees().map(employee => {
+      return employee.deliveries();
+    });
+    let merged = [].concat.apply([], allDeliveries);
+    return merged;
+  }
+  meals() {
+    let allMeals = this.deliveries().map(delivery => {
+      return delivery.meal();
+    });
+    let uniqueMeals = [...new Set(allMeals)];
+    return uniqueMeals;
   }
 }
